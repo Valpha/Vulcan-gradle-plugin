26b55b864583e1f88e4ee910c0760d5b31a43bf7
高源 <gaoyuan52@faw.com.cn>
2025-02-10 10:40:08 +0800

    临时提交，后续继续更新

--- src/main/kotlin/com/fawcar/artifacts/vulcan/VulcanRootProjectExtensions.kt
+++ src/main/kotlin/com/fawcar/artifacts/vulcan/VulcanRootProjectExtensions.kt
@@ -1,9 +1,23 @@
 package com.fawcar.artifacts.vulcan
 
+import com.android.build.api.dsl.ApplicationProductFlavor
+import com.android.build.api.dsl.CommonExtension
+import com.android.build.api.dsl.ProductFlavor
+import com.android.build.api.variant.AndroidComponentsExtension
+import org.gradle.api.Named
+import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Project
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.ListProperty
+import org.gradle.api.provider.Property
+import org.gradle.api.provider.SetProperty
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Internal
+import org.gradle.api.tasks.Nested
+import org.gradle.api.tasks.Optional
+import org.gradle.kotlin.dsl.container
 import org.gradle.kotlin.dsl.create
+import org.gradle.kotlin.dsl.setProperty
 
 interface VulcanRootProjectExtension {
 
@@ -14,20 +28,131 @@ interface VulcanRootProjectExtension {
     val outputXmlFile: RegularFileProperty
 }
 
+
+abstract class Flavor : Named {
+    @get:Input
+    abstract val modulePath: Property<String>
+
+    @get:Input
+    @get:Optional
+    abstract var ext: ((CommonExtension<*, *, *, *, *, *>).() -> Unit)?
+
+    @get:Input
+    @get:Optional
+    abstract var extension: (ProductFlavor.() -> Unit)?
+    override fun toString(): String {
+        return "(\"${modulePath.get()}\"${if (ext != null) ", ext" else ""}${if (extension != null) ", extension" else ""})"
+    }
+}
+
+abstract class FlavorDimension : Named {
+    @get:Input
+    abstract val targetModule: SetProperty<String>
+
+    @get:Nested
+    abstract val flavors: NamedDomainObjectContainer<Flavor>
+
+    var isSupportMulti: Boolean = false
+    var isMustChoose: Boolean = true
+
+    val allFlavors: Set<Flavor> get() = flavors.toSet()
+
+    override fun toString(): String {
+        return """
+        |{
+        |    name: ${name},
+        |    targetModule: ${targetModule.get()},
+        |    flavors: ${flavors.asMap},
+        |    isSupportMulti: $isSupportMulti,
+        |    isMustChoose: $isMustChoose
+        |}
+        """.trimMargin()
+    }
+}
+
+abstract class Variant : Named {
+    @get:Input
+    abstract val selectedFlavorNames: SetProperty<String>
+
+    @get:Internal
+    internal abstract val selectedFlavors: SetProperty<Flavor>
+
+    @get:Input
+    @get:Optional
+    abstract var configuration: (ApplicationProductFlavor.() -> Unit)?
+
+    override fun toString(): String {
+        return """
+        |{
+        |    name: ${name},
+        |    selectedFlavors: ${this.selectedFlavorNames.get()},
+        |    configuration: ${configuration?.run { "true" } ?: "null"}
+        |}
+        """.trimMargin()
+    }
+}
+
+open class VulcanConfigs(project: Project) {
+    val flavorDimension: NamedDomainObjectContainer<FlavorDimension> = project.container(FlavorDimension::class)
+    val variants: NamedDomainObjectContainer<Variant> = project.container(Variant::class)
+
+    val exclude: SetProperty<String> = project.objects.setProperty()
+    val app: SetProperty<String> = project.objects.setProperty()
+
+    val allFlavors get() = flavorDimension.flatMap { it.allFlavors }
+}
+
 internal fun Project.configureRootProjectVulcan() {
-    extensions.create<VulcanRootProjectExtension>("vulcan")
+    val vulcanExtension = extensions.create<VulcanRootProjectExtension>("vulcan")
+//
+    val vulcanConfigs = extensions.create<VulcanConfigs>("vulcan1", project)
 
     afterEvaluate {
         val tag = "afterEvaluate"
         "Dimensions defined. dimensions:$vulcanDimensionFlavors".log(tag)
 
-        // check Module exists
-        val allModules = subprojects.mapTo(HashSet()) { it.path }
+        "___DEBUG___.vulcanConfigs:flavors ${vulcanConfigs.flavorDimension.toList()}".log(tag)
+        "___DEBUG___.vulcanConfigs:variants ${vulcanConfigs.variants.toList()}".log(tag)
+
+        /**
+         * 所有的子模块
+         */
+        val allModules = subprojects.map { it.path }.toSet()
+
+        /**
+         * 所有的 Flavor 模块，等同于所有的差异实现层模块
+         */
+        val allFlavorsModulePath = vulcanConfigs.allFlavors.map { it.modulePath.get() }.toSet()
+
+        /**
+         * 所有的 FlavorDimension 目标模块，基本等同于接口代理层，差异化选择 flavor
+         */
+        val allTargetsModulePath = vulcanConfigs.flavorDimension.flatMap { it.targetModule.get() }.toSet()
+
+        /**
+         * 所有的排除模块，不参与 vulcan 配置
+         */
+        val allExcludedModules = vulcanConfigs.exclude.get()
+
+        /**
+         * app 顶层模块
+         */
+        val appModules = vulcanConfigs.app.get()
+
+        // check flavor Module exists
         allModules.containsAll(vulcanFlavorsModulePath).require(
             "Named flavor module ${vulcanFlavorsModulePath - allModules} was not found in project modules!"
         )
+        allModules.containsAll(allFlavorsModulePath).require(
+            "Named flavor module ${allFlavorsModulePath - allModules} was not found in project modules!"
+        )
 
-        // check platform variants exists
+        // check target module exists
+        allModules.containsAll(allTargetsModulePath).require(
+            "Dimension defined target module ${allTargetsModulePath - allModules} was not found in project modules!"
+        )
+
+        // check variants exists
         vulcanPlatformApplication.forEach {
             val platform = it.name
             val platformComponents = it.selectedFlavors
@@ -35,9 +160,14 @@ internal fun Project.configureRootProjectVulcan() {
                 "Platform \"$platform\" defined components ${platformComponents - vulcanFlavorsNames} was not found in defined Flavors!"
             )
             "Defined platform \"$platform\", with flavors: $platformComponents".log(tag)
-
         }
-
+        vulcanConfigs.variants.forEach {
+            val allSelectedFlavors = it.selectedFlavorNames.get()
+            vulcanConfigs.allFlavors.map { it.name }.toSet().containsAll(allSelectedFlavors).require(
+                "Variant \"${it.name}\" defined components ${allSelectedFlavors - vulcanFlavorsNames} was not found in defined Flavors!"
+            )
+            it.selectedFlavors.set(vulcanConfigs.allFlavors.filter { it.name in allSelectedFlavors }.toSet())
+        }
 
         /*tasks.create<GenerateFlavorDimensionsTask>("generateFlavorDimensions") {
             group = "build"
@@ -47,6 +177,37 @@ internal fun Project.configureRootProjectVulcan() {
             }
             dimensionXmlFile.set(rootProject.file("vulcan/dimensions.xml"))
         }*/
+
+        subprojects {
+            val tag = "ConfigSubProjects"
+            afterEvaluate {
+                "config Sub-Projects by rootProject".log(tag)
+            }
+            when (this.path) {
+                in allExcludedModules -> {
+                    "Exclude module ${this.path}".log(tag)
+                }
+
+                in appModules -> {
+                    "App module ${this.path}".log(tag)
+//                    extensions.getByType(AndroidComponentsExtension::class.java).let {
+//                        configureAndroidEntryModule(it)
+//                    }
+                }
+
+                in allFlavorsModulePath -> {
+                    "Flavor module ${this.path}".log(tag)
+                }
+
+                in allTargetsModulePath -> {
+                    "Target module ${this.path}".log(tag)
+                }
+
+                else -> {
+                    "Default business module ${this.path}".log(tag)
+                }
+            }
+        }
     }
 
 }
